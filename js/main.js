ae21d5a1fcc21a523c0321025fc8994104d94552
Yasir Khan <Yasirkahn6497@gmail.com>
2022-05-29 07:14:35 +0100

    YasirLab added!

--- js/main.js
+++ js/main.js
@@ -40,34 +40,47 @@ collapsibles.forEach((item) =>
 
 
 if(window.innerWidth<=650){ //--- if its mobile then set that menu link at top which is in viewport
-  console.log("reached");
+  // This object below "IntersectionObserver" is used to
+  // detect whether an element is in viewport
   var observer = new IntersectionObserver((sections)=>{
 
     
-    if(sections[0].isIntersecting === true)
+    if(sections[0].isIntersecting === true) //if its true, then element is visible in the veiwport
     menuNameChange(sections[0].target.id);
     
 
-},{threshold: [0.10]});
+},{threshold: [0.10]}); //based on this 2nd argument, we decide when will "isInterscting" property be true
+// 0 threshold means that element is visible slightly, 1 threshold means that element is fully visible
+// I set it to "0.10" because its working best for me here.
 
 for(let i=0; i<menu_sections.length; i++ )
-observer.observe(menu_sections[i]);
+observer.observe(menu_sections[i]); //Which element should be observed by this object that whether its showing in the veiwport or not
+// so i have applied on all the 3 sections on my webpage
 
 
 //------------ show the current viewed section menu link name at the top of menu-------
 
 function menuNameChange(target_sec){
  
-  let temp_section;
+  //in this function we check which section is showing in
+  //the veiwport and then we get the ID of the section and
+  // compare it with the target section which is currently
+   // observed by the "IntersectionObserver" object.
+   //if both equal, then we get the menu link using the 
+   //section name (while converting the string to lowercase cuz
+   //the id is in lowercase in html), and then we add that
+   //menu link at the top of the menu. 
+   //so when user scrolls down the menu links will be changed
+   //according to the current section that is veiwed in the
+   //veiw port. 
  
-  menu_sections.forEach(section=>{
-    console.log(target_sec);
+  menu_sections.forEach(section=>{ 
+    
     if(section.id == target_sec){
-      let name = (target_sec+"").toLowerCase();
-      let sec_list = menu.getElementsByClassName(name)[0];
-      let temp_sec = sec_list;
+      let sec_name = (target_sec+"").toLowerCase();
+      let sec_list = menu.getElementsByClassName(sec_name)[0];
         
-      menu.prepend(temp_sec);
+      menu.prepend(sec_list);
       
     }
   });
